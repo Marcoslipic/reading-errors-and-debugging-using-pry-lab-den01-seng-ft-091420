da385f0dfa4768064add66e7272e3a6fed8e209b
Marcos Lipic <mjlipic.37@gmail.com>
2020-09-01 18:04:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,7 +2,11 @@
 
 def snake_it_up(string)
   if string[0] == "s"
+<<<<<<< HEAD
     "ssssssssss" + string
+=======
+    (10 * "s".to_i).to_s + string
+>>>>>>> 43f93b51674e33c19682de31f160d7a5583862a0
   else
     string
   end
